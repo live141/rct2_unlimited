f080aa275bb07cf2d8e87147c40c70575995b2fd
Live <fabianf141@googlemail.com>
2014-08-25 17:42:18 +0200

    Fixed search algorithm and template errors

--- patch/memscan.h
+++ patch/memscan.h
@@ -3,50 +3,137 @@
 
 #include <vector>
 #include <stdint.h>
+#include <stdlib.h>
 
 #if defined(linux) || defined(__APPLE__)
-#define CHECK_BYTE(dst,src,flag) asm volatile("movaps %2, %%xmm0\n\t" \
-		"movups %1, %%xmm1\n\t" \
+#define CHECK_BYTE(dst,src,flag) asm volatile("movaps (%2), %%xmm0\n\t" \
+		"movups (%1), %%xmm1\n\t" \
 		"pcmpeqb %%xmm0, %%xmm1\n\t" \
 		"movaps %%xmm1, %%xmm3\n\t" \
 		"pshufd $0xee, %%xmm1, %%xmm3\n\t" \
 		"paddq %%xmm1, %%xmm3\n\t" \
 		"pextrq $0, %%xmm3, %0\n\t" \
 		: "=m"(flag) \
-		: "m"(dst), "m"(src) \
+		: "r"(dst), "r"(src) \
 		: "xmm0", "xmm1", "xmm3")
 
-#define CHECK_WORD(dst,src,flag) asm volatile("movaps %2, %%xmm0\n\t" \
-		"movups %1, %%xmm1\n\t" \
+#define CHECK_WORD(dst,src,flag) asm volatile("movaps (%2), %%xmm0\n\t" \
+		"movups (%1), %%xmm1\n\t" \
 		"pcmpeqw %%xmm0, %%xmm1\n\t" \
 		"movaps %%xmm1, %%xmm3\n\t" \
 		"pshufd $0xee, %%xmm1, %%xmm3\n\t" \
 		"paddq %%xmm1, %%xmm3\n\t" \
 		"pextrq $0, %%xmm3, %0\n\t" \
 		: "=m"(flag) \
-		: "m"(dst), "m"(src) \
+		: "r"(dst), "r"(src) \
 		: "xmm0", "xmm1", "xmm3")
 
-#define CHECK_DWORD(dst,src,flag) asm volatile("movaps %2, %%xmm0\n\t" \
-		"movups %1, %%xmm1\n\t" \
+#define CHECK_DWORD(dst,src,flag) asm volatile("movaps (%2), %%xmm0\n\t" \
+		"movups (%1), %%xmm1\n\t" \
 		"pcmpeqd %%xmm0, %%xmm1\n\t" \
 		"movaps %%xmm1, %%xmm3\n\t" \
 		"pshufd $0xee, %%xmm1, %%xmm3\n\t" \
 		"paddq %%xmm1, %%xmm3\n\t" \
 		"pextrq $0, %%xmm3, %0\n\t" \
 		: "=m"(flag) \
-		: "m"(dst), "m"(src) \
+		: "r"(dst), "r"(src) \
 		: "xmm0", "xmm1", "xmm3")
 
-#define CHECK_QWORD(dst,src,flag) asm volatile("movaps %2, %%xmm0\n\t" \
-		"movups %1, %%xmm1\n\t" \
+#define CHECK_QWORD(dst,src,flag) asm volatile("movaps (%2), %%xmm0\n\t" \
+		"movups (%1), %%xmm1\n\t" \
 		"pcmpeqq %%xmm0, %%xmm1\n\t" \
 		"movaps %%xmm1, %%xmm3\n\t" \
 		"pshufd $0xee, %%xmm1, %%xmm3\n\t" \
 		"paddq %%xmm1, %%xmm3\n\t" \
 		"pextrq $0, %%xmm3, %0\n\t" \
 		: "=m"(flag) \
-		: "m"(dst), "m"(src) \
+		: "r"(dst), "r"(src) \
+		: "xmm0", "xmm1", "xmm3")
+#define CHECK_BYTE_GT(dst,src,flag) asm volatile("movaps (%2), %%xmm0\n\t" \
+		"movups (%1), %%xmm1\n\t" \
+		"pcmpgtb %%xmm0, %%xmm1\n\t" \
+		"movaps %%xmm1, %%xmm3\n\t" \
+		"pshufd $0xee, %%xmm1, %%xmm3\n\t" \
+		"paddq %%xmm1, %%xmm3\n\t" \
+		"pextrq $0, %%xmm3, %0\n\t" \
+		: "=m"(flag) \
+		: "r"(dst), "r"(src) \
+		: "xmm0", "xmm1", "xmm3")
+
+#define CHECK_WORD_GT(dst,src,flag) asm volatile("movaps (%2), %%xmm0\n\t" \
+		"movups (%1), %%xmm1\n\t" \
+		"pcmpgtw %%xmm0, %%xmm1\n\t" \
+		"movaps %%xmm1, %%xmm3\n\t" \
+		"pshufd $0xee, %%xmm1, %%xmm3\n\t" \
+		"paddq %%xmm1, %%xmm3\n\t" \
+		"pextrq $0, %%xmm3, %0\n\t" \
+		: "=m"(flag) \
+		: "r"(dst), "r"(src) \
+		: "xmm0", "xmm1", "xmm3")
+
+#define CHECK_DWORD_GT(dst,src,flag) asm volatile("movaps (%2), %%xmm0\n\t" \
+		"movups (%1), %%xmm1\n\t" \
+		"pcmpgtd %%xmm0, %%xmm1\n\t" \
+		"movaps %%xmm1, %%xmm3\n\t" \
+		"pshufd $0xee, %%xmm1, %%xmm3\n\t" \
+		"paddq %%xmm1, %%xmm3\n\t" \
+		"pextrq $0, %%xmm3, %0\n\t" \
+		: "=m"(flag) \
+		: "r"(dst), "r"(src) \
+		: "xmm0", "xmm1", "xmm3")
+
+#define CHECK_QWORD_GT(dst,src,flag) asm volatile("movaps (%2), %%xmm0\n\t" \
+		"movups (%1), %%xmm1\n\t" \
+		"pcmpgtq %%xmm0, %%xmm1\n\t" \
+		"movaps %%xmm1, %%xmm3\n\t" \
+		"pshufd $0xee, %%xmm1, %%xmm3\n\t" \
+		"paddq %%xmm1, %%xmm3\n\t" \
+		"pextrq $0, %%xmm3, %0\n\t" \
+		: "=m"(flag) \
+		: "r"(dst), "r"(src) \
+		: "xmm0", "xmm1", "xmm3")
+#define CHECK_BYTE_LE(dst,src,flag) asm volatile("movaps (%2), %%xmm1\n\t" \
+		"movups (%1), %%xmm0\n\t" \
+		"pcmpgtb %%xmm0, %%xmm1\n\t" \
+		"movaps %%xmm1, %%xmm3\n\t" \
+		"pshufd $0xee, %%xmm1, %%xmm3\n\t" \
+		"paddq %%xmm1, %%xmm3\n\t" \
+		"pextrq $0, %%xmm3, %0\n\t" \
+		: "=m"(flag) \
+		: "r"(dst), "r"(src) \
+		: "xmm0", "xmm1", "xmm3")
+
+#define CHECK_WORD_LE(dst,src,flag) asm volatile("movaps (%2), %%xmm1\n\t" \
+		"movups (%1), %%xmm0\n\t" \
+		"pcmpgtw %%xmm0, %%xmm1\n\t" \
+		"movaps %%xmm1, %%xmm3\n\t" \
+		"pshufd $0xee, %%xmm1, %%xmm3\n\t" \
+		"paddq %%xmm1, %%xmm3\n\t" \
+		"pextrq $0, %%xmm3, %0\n\t" \
+		: "=m"(flag) \
+		: "r"(dst), "r"(src) \
+		: "xmm0", "xmm1", "xmm3")
+
+#define CHECK_DWORD_LE(dst,src,flag) asm volatile("movaps (%2), %%xmm1\n\t" \
+		"movups (%1), %%xmm0\n\t" \
+		"pcmpgtd %%xmm0, %%xmm1\n\t" \
+		"movaps %%xmm1, %%xmm3\n\t" \
+		"pshufd $0xee, %%xmm1, %%xmm3\n\t" \
+		"paddq %%xmm1, %%xmm3\n\t" \
+		"pextrq $0, %%xmm3, %0\n\t" \
+		: "=m"(flag) \
+		: "r"(dst), "r"(src) \
+		: "xmm0", "xmm1", "xmm3")
+
+#define CHECK_QWORD_LE(dst,src,flag) asm volatile("movaps (%2), %%xmm1\n\t" \
+		"movups (%1), %%xmm0\n\t" \
+		"pcmpgtq %%xmm0, %%xmm1\n\t" \
+		"movaps %%xmm1, %%xmm3\n\t" \
+		"pshufd $0xee, %%xmm1, %%xmm3\n\t" \
+		"paddq %%xmm1, %%xmm3\n\t" \
+		"pextrq $0, %%xmm3, %0\n\t" \
+		: "=m"(flag) \
+		: "r"(dst), "r"(src) \
 		: "xmm0", "xmm1", "xmm3")
 #else
 #define CHECK_BYTE(dst,src,flag) __asm__ volatile { \
@@ -188,19 +275,469 @@ protected:
 #endif
 	std::vector<T*> _vec_results;
 
+	uint32_t _search_byte(T val, T *begin, T *end, search_type type) {
+		uint64_t found = 0;
+		/* move value to our aligned member variable */
+		for(uint8_t j = 0; j < 16/sizeof(T); ++j) {
+			_val[j] = val;
+		}
+
+		switch(type) {
+			case search_equal:
+				for(T *i = begin; i < end; i += 16) {
+					/* check against every possible alignment */
+					for(uint8_t j = 0; j < sizeof(T); ++j) {
+						CHECK_BYTE(i+j, _val, found);
+
+						if( found != 0 ) {
+							/* where exactly? */
+							for(uint8_t k = 0; k < 17-sizeof(T); k += sizeof(T)) {
+								if(*((T*) ((uint8_t*)i+j+k)) == _val[0])
+									_vec_results.push_back((T*) ((uint8_t*)i+j+k));
+							}
+						}
+					}
+				}
+				break;
+			case search_gt:
+				for(T *i = begin; i < end; i += 16) {
+					/* check against every possible alignment */
+					for(uint8_t j = 0; j < sizeof(T); ++j) {
+						CHECK_BYTE_GT(i+j, _val, found);
+
+						if( found != 0 ) {
+							/* where exactly? */
+							for(uint8_t k = 0; k < 17-sizeof(T); k += sizeof(T)) {
+								if(*((T*) ((uint8_t*)i+j+k)) > _val[0])
+									_vec_results.push_back((T*) ((uint8_t*)i+j+k));
+							}
+						}
+					}
+				}
+				break;
+			case search_ge:
+				for(T *i = begin; i < end; i += 16) {
+					/* check against every possible alignment */
+					for(uint8_t j = 0; j < sizeof(T); ++j) {
+						CHECK_BYTE_GT(i+j, _val-1, found);
+
+						if( found != 0 ) {
+							/* where exactly? */
+							for(uint8_t k = 0; k < 17-sizeof(T); k += sizeof(T)) {
+								if(*((T*) ((uint8_t*)i+j+k)) >= _val[0])
+									_vec_results.push_back((T*) ((uint8_t*)i+j+k));
+							}
+						}
+					}
+				}
+				break;
+			case search_lt:
+				for(T *i = begin; i < end; i += 16) {
+					/* check against every possible alignment */
+					for(uint8_t j = 0; j < sizeof(T); ++j) {
+						CHECK_BYTE_GT(_val, i+j,found);
+
+						if( found != 0 ) {
+							/* where exactly? */
+							for(uint8_t k = 0; k < 17-sizeof(T); k += sizeof(T)) {
+								if(*((T*) ((uint8_t*)i+j+k)) < _val[0])
+									_vec_results.push_back((T*) ((uint8_t*)i+j+k));
+							}
+						}
+					}
+				}
+				break;
+			case search_le:
+				for(T *i = begin; i < end; i += 16) {
+					/* check against every possible alignment */
+					for(uint8_t j = 0; j < sizeof(T); ++j) {
+						CHECK_BYTE_GT(_val+1, i+j, found);
+
+						if( found != 0 ) {
+							/* where exactly? */
+							for(uint8_t k = 0; k < 17-sizeof(T); k += sizeof(T)) {
+								if(*((T*) ((uint8_t*)i+j+k)) <= _val[0])
+									_vec_results.push_back((T*) ((uint8_t*)i+j+k));
+							}
+						}
+					}
+				}
+				break;
+			default:
+				break;
+		}
+
+		return _vec_results.size();
+	}
+
+	uint32_t _search_word(T val, T *begin, T *end, search_type type) {
+		uint64_t found = 0;
+		/* move value to our aligned member variable */
+		for(uint8_t j = 0; j < 16/sizeof(T); ++j) {
+			_val[j] = val;
+		}
+
+		switch(type) {
+			case search_equal:
+				for(uint8_t *i = (uint8_t*) begin; i < (uint8_t*) end-16; i += 16) {
+					/* check against every possible alignment */
+					for(uint8_t j = 0; j < sizeof(T); ++j) {
+						CHECK_WORD(i+j, _val, found);
+
+						if( found != 0 ) {
+							/* where exactly? */
+							for(uint8_t k = 0; k < 17-sizeof(T); k += sizeof(T)) {
+								if(*((T*) ((uint8_t*)i+j+k)) == _val[0])
+									_vec_results.push_back((T*) ((uint8_t*)i+j+k));
+							}
+						}
+					}
+				}
+				break;
+			case search_gt:
+				for(uint8_t *i = (uint8_t*) begin; i < (uint8_t*) end-16; i += 16) {
+					/* check against every possible alignment */
+					for(uint8_t j = 0; j < sizeof(T); ++j) {
+						CHECK_WORD_GT(i+j, _val, found);
+
+						if( found != 0 ) {
+							/* where exactly? */
+							for(uint8_t k = 0; k < 17-sizeof(T); k += sizeof(T)) {
+								if(*((T*) ((uint8_t*)i+j+k)) > _val[0])
+									_vec_results.push_back((T*) ((uint8_t*)i+j+k));
+							}
+						}
+					}
+				}
+				break;
+			case search_ge:
+				for(uint8_t *i = (uint8_t*) begin; i < (uint8_t*) end-16; i += 16) {
+					/* check against every possible alignment */
+					for(uint8_t j = 0; j < sizeof(T); ++j) {
+						CHECK_WORD_GT(i+j, _val-1, found);
+
+						if( found != 0 ) {
+							/* where exactly? */
+							for(uint8_t k = 0; k < 17-sizeof(T); k += sizeof(T)) {
+								if(*((T*) ((uint8_t*)i+j+k)) >= _val[0])
+									_vec_results.push_back((T*) ((uint8_t*)i+j+k));
+							}
+						}
+					}
+				}
+				break;
+			case search_lt:
+				for(uint8_t *i = (uint8_t*) begin; i < (uint8_t*) end-16; i += 16) {
+					/* check against every possible alignment */
+					for(uint8_t j = 0; j < sizeof(T); ++j) {
+						CHECK_WORD_GT(_val, i+j,found);
+
+						if( found != 0 ) {
+							/* where exactly? */
+							for(uint8_t k = 0; k < 17-sizeof(T); k += sizeof(T)) {
+								if(*((T*) ((uint8_t*)i+j+k)) < _val[0])
+									_vec_results.push_back((T*) ((uint8_t*)i+j+k));
+							}
+						}
+					}
+				}
+				break;
+			case search_le:
+				for(uint8_t *i = (uint8_t*) begin; i < (uint8_t*) end-16; i += 16) {
+					/* check against every possible alignment */
+					for(uint8_t j = 0; j < sizeof(T); ++j) {
+						CHECK_WORD_GT(_val+1, i+j, found);
+
+						if( found != 0 ) {
+							/* where exactly? */
+							for(uint8_t k = 0; k < 17-sizeof(T); k += sizeof(T)) {
+								if(*((T*) ((uint8_t*)i+j+k)) <= _val[0])
+									_vec_results.push_back((T*) ((uint8_t*)i+j+k));
+							}
+						}
+					}
+				}
+				break;
+			default:
+				break;
+		}
+
+		return _vec_results.size();
+	}
+
+	uint32_t _search_dword(T val, T *begin, T *end, search_type type) {
+		uint64_t found = 0;
+		/* move value to our aligned member variable */
+		for(uint8_t j = 0; j < 16/sizeof(T); ++j) {
+			_val[j] = val;
+		}
+
+		switch(type) {
+			case search_equal:
+				for(uint8_t *i = (uint8_t*) begin; i < (uint8_t*) end-16; i += 16) {
+					/* check against every possible alignment */
+					for(uint8_t j = 0; j < sizeof(T); ++j) {
+						CHECK_DWORD(i+j, _val, found);
+
+						if( found != 0 ) {
+							/* where exactly? */
+							for(uint8_t k = 0; k < 17-sizeof(T); k += sizeof(T)) {
+								if(*((T*) ((uint8_t*)i+j+k)) == _val[0])
+									_vec_results.push_back((T*) ((uint8_t*)i+j+k));
+							}
+						}
+					}
+				}
+				break;
+			case search_gt:
+				for(uint8_t *i = (uint8_t*) begin; i < (uint8_t*) end-16; i += 16) {
+					/* check against every possible alignment */
+					for(uint8_t j = 0; j < sizeof(T); ++j) {
+						CHECK_DWORD_GT(i+j, _val, found);
+
+						if( found != 0 ) {
+							/* where exactly? */
+							for(uint8_t k = 0; k < 17-sizeof(T); k += sizeof(T)) {
+								if(*((T*) ((uint8_t*)i+j+k)) > _val[0])
+									_vec_results.push_back((T*) ((uint8_t*)i+j+k));
+							}
+						}
+					}
+				}
+				break;
+			case search_ge:
+				for(uint8_t *i = (uint8_t*) begin; i < (uint8_t*) end-16; i += 16) {
+					/* check against every possible alignment */
+					for(uint8_t j = 0; j < sizeof(T); ++j) {
+						CHECK_DWORD_GT(i+j, _val-1, found);
+
+						if( found != 0 ) {
+							/* where exactly? */
+							for(uint8_t k = 0; k < 17-sizeof(T); k += sizeof(T)) {
+								if(*((T*) ((uint8_t*)i+j+k)) >= _val[0])
+									_vec_results.push_back((T*) ((uint8_t*)i+j+k));
+							}
+						}
+					}
+				}
+				break;
+			case search_lt:
+				for(uint8_t *i = (uint8_t*) begin; i < (uint8_t*) end-16; i += 16) {
+					/* check against every possible alignment */
+					for(uint8_t j = 0; j < sizeof(T); ++j) {
+						CHECK_DWORD_GT(_val, i+j,found);
+
+						if( found != 0 ) {
+							/* where exactly? */
+							for(uint8_t k = 0; k < 17-sizeof(T); k += sizeof(T)) {
+								if(*((T*) ((uint8_t*)i+j+k)) < _val[0])
+									_vec_results.push_back((T*) ((uint8_t*)i+j+k));
+							}
+						}
+					}
+				}
+				break;
+			case search_le:
+				for(uint8_t *i = (uint8_t*) begin; i < (uint8_t*) end-16; i += 16) {
+					/* check against every possible alignment */
+					for(uint8_t j = 0; j < sizeof(T); ++j) {
+						CHECK_DWORD_GT(_val+1, i+j, found);
+
+						if( found != 0 ) {
+							/* where exactly? */
+							for(uint8_t k = 0; k < 17-sizeof(T); k += sizeof(T)) {
+								if(*((T*) ((uint8_t*)i+j+k)) <= _val[0])
+									_vec_results.push_back((T*) ((uint8_t*)i+j+k));
+							}
+						}
+					}
+				}
+				break;
+			default:
+				break;
+		}
+
+		return _vec_results.size();
+	}
+
+	uint32_t _search_qword(T val, T *begin, T *end, search_type type) {
+		uint64_t found = 0;
+		/* move value to our aligned member variable */
+		for(uint8_t j = 0; j < 16/sizeof(T); ++j) {
+			_val[j] = val;
+		}
+
+		switch(type) {
+			case search_equal:
+				for(uint8_t *i = (uint8_t*) begin; i < (uint8_t*) end-16; i += 16) {
+					/* check against every possible alignment */
+					for(uint8_t j = 0; j < sizeof(T); ++j) {
+						CHECK_QWORD(i+j, _val, found);
+
+						if( found != 0 ) {
+							/* where exactly? */
+							for(uint8_t k = 0; k < 17-sizeof(T); k += sizeof(T)) {
+								if(*((T*) ((uint8_t*)i+j+k)) == _val[0])
+									_vec_results.push_back((T*) ((uint8_t*)i+j+k));
+							}
+						}
+					}
+				}
+				break;
+			case search_gt:
+				for(uint8_t *i = (uint8_t*) begin; i < (uint8_t*) end-16; i += 16) {
+					/* check against every possible alignment */
+					for(uint8_t j = 0; j < sizeof(T); ++j) {
+						CHECK_QWORD_GT(i+j, _val, found);
+
+						if( found != 0 ) {
+							/* where exactly? */
+							for(uint8_t k = 0; k < 17-sizeof(T); k += sizeof(T)) {
+								if(*((T*) ((uint8_t*)i+j+k)) > _val[0])
+									_vec_results.push_back((T*) ((uint8_t*)i+j+k));
+							}
+						}
+					}
+				}
+				break;
+			case search_ge:
+				for(uint8_t *i = (uint8_t*) begin; i < (uint8_t*) end-16; i += 16) {
+					/* check against every possible alignment */
+					for(uint8_t j = 0; j < sizeof(T); ++j) {
+						CHECK_QWORD_GT(i+j, _val-1, found);
+
+						if( found != 0 ) {
+							/* where exactly? */
+							for(uint8_t k = 0; k < 17-sizeof(T); k += sizeof(T)) {
+								if(*((T*) ((uint8_t*)i+j+k)) >= _val[0])
+									_vec_results.push_back((T*) ((uint8_t*)i+j+k));
+							}
+						}
+					}
+				}
+				break;
+			case search_lt:
+				for(uint8_t *i = (uint8_t*) begin; i < (uint8_t*) end-16; i += 16) {
+					/* check against every possible alignment */
+					for(uint8_t j = 0; j < sizeof(T); ++j) {
+						CHECK_QWORD_GT(_val, i+j,found);
+
+						if( found != 0 ) {
+							/* where exactly? */
+							for(uint8_t k = 0; k < 17-sizeof(T); k += sizeof(T)) {
+								if(*((T*) ((uint8_t*)i+j+k)) < _val[0])
+									_vec_results.push_back((T*) ((uint8_t*)i+j+k));
+							}
+						}
+					}
+				}
+				break;
+			case search_le:
+				for(uint8_t *i = (uint8_t*) begin; i < (uint8_t*) end-16; i += 16) {
+					/* check against every possible alignment */
+					for(uint8_t j = 0; j < sizeof(T); ++j) {
+						CHECK_QWORD_GT(_val+1, i+j, found);
+
+						if( found != 0 ) {
+							/* where exactly? */
+							for(uint8_t k = 0; k < 17-sizeof(T); k += sizeof(T)) {
+								if(*((T*) ((uint8_t*)i+j+k)) <= _val[0])
+									_vec_results.push_back((T*) ((uint8_t*)i+j+k));
+							}
+						}
+					}
+				}
+				break;
+			default:
+				break;
+		}
+		return _vec_results.size();
+	}
+
 public:
+	typedef typename std::vector<T*>::iterator iterator;
 	memscan() {}
-	uint32_t search_byte(T val, T *begin, T *end, search_type type);
-	uint32_t search_word(T val, T *begin, T *end, search_type type);
-	uint32_t search_dword(T val, T *begin, T *end, search_type type);
-	uint32_t search_qword(T val, T *begin, T *end, search_type type);
 
+	iterator begin() {
+		return _vec_results.begin();
+	}
 
-	uint32_t search_cont(search_type type, T val = 0);
+	iterator end() {
+		return _vec_results.end();
+	}
 
-	std::vector<T*>& results() {
-		return _vec_results;
+	size_t size() const {
+		return _vec_results.size();
+	}
+
+	uint32_t search(T val, void *begin, void *end, search_type type) {
+		switch(sizeof(T)) {
+			case 1:
+				return _search_byte(val, (T*) begin, (T*) end, type);
+				break;
+			case 2:
+				return _search_word(val, (T*) begin, (T*) end, type);
+				break;
+			case 4:
+				return _search_dword(val, (T*) begin, (T*) end, type);
+				break;
+			case 8:
+				return _search_qword(val, (T*) begin, (T*) end, type);
+				break;
+		}
+	}
+
+	uint32_t search_cont(T val, search_type type) {
+		typename std::vector<T*> vec;
+		typename std::vector<T*>::iterator it;
+		switch(type) {
+			case search_equal:
+				for(it = _vec_results.begin(); it < _vec_results.end(); ++it) {
+					if( **it == val )
+						vec.push_back(*it);
+				}
+				break;
+			case search_notequal:
+				for(it = _vec_results.begin(); it < _vec_results.end(); ++it) {
+					if( **it != val )
+						vec.push_back(*it);
+				}
+				break;
+			case search_gt:
+				for(it = _vec_results.begin(); it < _vec_results.end(); ++it) {
+					if( **it > val )
+						vec.push_back(*it);
+				}
+				break;
+			case search_ge:
+				for(it = _vec_results.begin(); it < _vec_results.end(); ++it) {
+					if( **it >= val )
+						vec.push_back(*it);
+				}
+				break;
+			case search_lt:
+				for(it = _vec_results.begin(); it < _vec_results.end(); ++it) {
+					if( **it < val )
+						vec.push_back(*it);
+				}
+				break;
+			case search_le:
+				for(it = _vec_results.begin(); it < _vec_results.end(); ++it) {
+					if( **it <= val )
+						vec.push_back(*it);
+				}
+				break;
+			case search_unchanged:
+				for(it = _vec_results.begin(); it < _vec_results.end(); ++it) {
+					if( **it == _val )
+						vec.push_back(*it);
+				}
+				break;
+			default:
+				break;
+		}
+
+		_vec_results = vec;
 	}
 };
-
 #endif
